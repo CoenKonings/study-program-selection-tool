56a2d6dc0d8526ea2dd8d976f9a7819ed49df243
Coen Konings <coen.konings@hotmail.com>
2023-06-04 13:36:26 +0200

    add score and criterium admin

--- backend/dss/admin.py
+++ backend/dss/admin.py
@@ -22,7 +22,17 @@ class AnswerAdmin(admin.ModelAdmin):
     list_display = ["__str__", "id"]
 
 
+class CriteriumAdmin(admin.ModelAdmin):
+    list_display = ["id", "description"]
+
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ["criterium", "study_program", "value"]
+
+
 admin.site.register(StudyProgram, StudyProgramAdmin)
 admin.site.register(Node, NodeAdmin)
 admin.site.register(DecisionTree, DecisionTreeAdmin)
 admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Criterium, CriteriumAdmin)
+admin.site.register(Score, ScoreAdmin)
