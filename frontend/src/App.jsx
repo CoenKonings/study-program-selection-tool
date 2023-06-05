081061104569d77370e8c99ba6d8a77ac8be5874
Coen Konings <coen.konings@hotmail.com>
2023-06-05 13:27:48 +0200

    allow criteria selection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 /**
@@ -8,11 +8,10 @@ function App() {
   return (
     <>
       <div>
-        <h1>Study Program Selection Support</h1>
         <SystemSelector />
       </div>
     </>
-  )
+  );
 }
 
 /**
@@ -29,24 +28,25 @@ function SystemSelector() {
   if (system === 0) {
     return <DecisionTree
       reset={reset}
-    />
+    />;
   } else if (system === 1) {
     return <PawPaw
       reset={reset}
-    />
+    />;
   } else if (system === 2) {
     return <ConversationalSystem
-     reset={reset}
-    />
+      reset={reset}
+    />;
   }
 
   return (
     <>
+      <h1>Selecteer een systeem</h1>
       <button onClick={() => setSystem(0)}>Decision Tree</button>
       <button onClick={() => setSystem(1)}>PAW-PAW</button>
       <button onClick={() => setSystem(2)}>Conversational System</button>
     </>
-  )
+  );
 }
 
 /**
@@ -54,12 +54,31 @@ function SystemSelector() {
  * system.
 */
 function PawPaw({ reset }) {
+  let [criteria, setCriteria] = useState([]);
+  let [selectedCriteria, setSelectedCriteria] = useState([]);
+
+  const fetchCriteria = () => fetch("http://localhost:8000/criteria/")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      setCriteria(data);
+    });
+
+    useEffect(() => {
+      fetchCriteria();
+    }, []);
+
   return (
     <>
       <h2>PAW-PAW</h2>
+      <CriteriaSelector
+        criteria={criteria}
+        setSelectedCriteria={setSelectedCriteria}
+      />
       <button onClick={reset}>Back to menu</button>
     </>
-  )
+  );
 }
 
 /**
@@ -72,7 +91,7 @@ function DecisionTree({ reset }) {
       <h2>Decision Tree</h2>
       <button onClick={reset}>Back to menu</button>
     </>
-  )
+  );
 }
 
 /**
@@ -85,7 +104,56 @@ function ConversationalSystem({ reset }) {
       <h2>Conversational System</h2>
       <button onClick={reset}>Back to menu</button>
     </>
-  )
+  );
 }
 
-export default App
+/**
+ * A component that allows the user to select which criteria to use in PAW-PAW.
+ */
+function CriteriaSelector({ criteria, setSelectedCriteria }) {
+  let [nQuestions, setNQuestions] = useState(0);
+  let [checkedCriteria, setCheckedCriteria] = useState([]);
+
+  // Recalculate the number of questions every time "checkedCriteria" is
+  // updated.
+  useEffect(() => {
+    let sum = 0;
+    for (let i = 0; i < checkedCriteria.length; i++) {
+      sum += i;
+    }
+
+    setNQuestions(sum);
+  }, [checkedCriteria]);
+
+  const handleCheck = (event, criterium) => {
+    if (event.target.checked) {
+      setCheckedCriteria([...checkedCriteria, criterium]);
+    } else {
+      setCheckedCriteria(checkedCriteria.filter(c => c !== criterium));
+    }
+  }
+
+  // TODO: submit form -> set state in PawPaw component.
+  return (
+    <>
+      <h2>Selecteer de criteria die je wil gebruiken.</h2>
+      <p>Aantal vragen dat je gesteld gaat worden: {nQuestions}</p>
+      <p>Aantal criteria: {checkedCriteria.length}</p>
+      <form className="criteria-form">
+        {criteria.map(criterium => (
+          <label key={criterium.id}>
+            <input
+              type="checkbox"
+              name={"criterium-" + criterium.id}
+              id={"criterium-" + criterium.id + "-select"}
+              onChange={(e) => handleCheck(e, criterium)}
+            />
+            {criterium.description}
+          </label>
+        ))}
+      </form>
+    </>
+  );
+}
+
+export default App;
