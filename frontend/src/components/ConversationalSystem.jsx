a426db4996a367c886074cfc87e037889ad648b6
Coen Konings <coen.konings@hotmail.com>
2023-06-13 12:05:37 +0200

    show chat history

--- frontend/src/components/ConversationalSystem.jsx
+++ frontend/src/components/ConversationalSystem.jsx
@@ -5,11 +5,34 @@ import { useState, useEffect } from 'react';
  * support system.
  */
 function ConversationalSystem() {
+  let [messages, setMessages] = useState([]);
+
+  const fetchMessage = () => {
+    setMessages([...messages, "bericht" + messages.length]);
+  }
+
+  useEffect(() => {
+    fetchMessage();
+  }, []);
+
   return (
     <>
       <h2>Conversational System</h2>
+      <ChatHistory
+        messages={messages}
+      />
     </>
   );
+}
+
+function ChatHistory({ messages }) {
+  return (
+    <div className='chat-history'>
+      {messages.map((message, index) => (
+        <p className='message' key={'message-' + index}>{message}</p>
+      ))}
+    </div>
+  )
 }
 
 export default ConversationalSystem;
